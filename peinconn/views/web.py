453b8d724bef303778a6e4235476a117c88d9889
Gods-own <oritzidumeka@gmail.com>
2022-09-13 22:56:34 +0100

    add interest

--- peinconn/views/web.py
+++ peinconn/views/web.py
@@ -31,7 +31,7 @@ def login():
             the_model_field = User.query.filter_by(username=form.username.data).first()
 
             if not check_password_hash(the_model_field.password, form.password.data):
-                return redirect(url_for('login', message='Incorrect Password'))
+                return render_template("login.html", form=form, message='Incorrect passwordd')    
             else:
 
                 session["user_id"] = the_model_field.id
@@ -72,3 +72,32 @@ def register():
     else:    
         form = RegistrationForm()
         return render_template("register.html", form=form)
+
+@web.route('/add-interests', methods=['GET', 'POST'])
+def registerInterest():
+    # countries = dict(countries_for_language('en'))
+
+    # print([(key, countries[key]) for key in countries.keys()])
+    if request.method == "POST":
+        form = RegistrationForm()
+        print(form.password.data, form.country.data)
+        if form.validate_on_submit():
+            password = generate_password_hash(form.password.data)
+
+            country = acc_for_uniqueness(Country, {'country': form.country.data}, country=form.country.data)
+            print(country)
+
+            user = User(username=form.username.data, name=form.name.data, email=form.email.data, password=password, gender=form.gender.data, date_of_birth=form.date_of_birth.data)
+            country.country_users.append(user)
+            # country.user = [user]
+
+            # db.session.add(user)
+            db.session.add(country)
+            db.session.commit()
+
+            return redirect('/')
+        else:    
+            return render_template("register.html", form=form)
+    else:    
+        form = RegistrationForm()
+        return render_template("register.html", form=form)        
