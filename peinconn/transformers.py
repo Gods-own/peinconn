50245c1763a09e5a0107d082b059434262fe9c1f
idumeka <idumeka.oritogun@dreamlabs.com.ng>
2022-10-07 16:56:59 +0100

    hjdfhbf

--- peinconn/transformers.py
+++ peinconn/transformers.py
@@ -5,10 +5,43 @@ class UserSchema(ma.Schema):
         fields = ('id', 'username', 'email', 'introduction', 'gender', 'date_of_birth', 'userImage', 'is_admin', 'is_active', 
         'date_joined', 'last_login', 'interests', 'country', 'created_At', 'updated_At')
 
-        interests = ma.Nested(InterestSchema)
+        interests = ma.Nested("InterestSchema")
+        country = ma.Nested("CountrySchema") 
 
 class CountrySchema(ma.Schema):
     class Meta:
-        fields = ('id', 'country', 'created_At', 'updated_At')
+        fields = ('id', 'country', 'created_At', 'updated_At')     
 
-        interests = ma.Nested(InterestSchema)        
+class InterestSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'hobby', 'created_At', 'updated_At')    
+
+class ActivitySchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'user_id', 'user', 'activity', 'picture', 'interest', 'like_no', 'created_At', 'updated_At')    
+
+        user = ma.Nested("UserSchema")
+        interest = ma.Nested("InterestSchema")  
+
+        # links = ma.Hyperlinks({
+        #     'firstPage':
+        #     'lastPage':
+        #     'prevPage':
+        #     'nextPage':
+        #     'meta': {
+        #         'paging': {
+        #         'paegCount':
+        #         'totalPages':
+        #         'page':
+        #         'hasPrevPage':
+        #         'hasNextPage':
+        #         }
+        #     }
+        # })
+
+class LikedSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'user_id', 'user', 'activity_id', 'activity', 'created_At', 'updated_At')
+
+        user = ma.Nested("UserSchema")
+        activity = ma.Nested("ActivitySchema", exclude("user",)) 
